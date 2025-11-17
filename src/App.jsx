7bc9fee0d95f09d8777ff89a5f5d01cfd6a4bc0e
YOHANROSHITHA <yohanroshitha6@gmail.com>
2025-11-17 17:37:47 +0530

    Implement dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Pages/Home'
 import Header from './Components/Header'
+import { ThemeContext } from './Context/ThemeContext'
+
 
 function App() {
   const [count, setCount] = useState(0)
-  const [theme,setTheme] = useState('light')
+  const [theme,setTheme] = useState('dark')
 
   return (
-    
-    <div className={ `${theme} ${theme=='dark' ?'bg-[#121212]' : null} `}>
+
+    <ThemeContext.Provider value={{theme,setTheme}}>
+    <div className={ `${theme} ${theme=='dark' ?'bg-[#121212]' : null} h-[100vh] `}>
     <h1 class="App">
       <Header/>
       <Home/>
     </h1>
-
-    
-    </div>
+  </div>
+  </ThemeContext.Provider>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import logo from './../assets/images/logo.jpg'
 import { IoMdSearch } from "react-icons/io";
 import { IoMoon } from "react-icons/io5";
 import { MdSunny } from "react-icons/md";
+import { ThemeContext } from "../Context/ThemeContext";
 
 
 function Header() {
     const [toggle, setToggle] = useState(true)
+    const {theme,setTheme}=useContext(ThemeContext);
+
+    useEffect(() => {
+      console.log("Theme", theme)
+    },[theme])
   return (
     <div className='flex items-center gap-2 p-4'>
       <img src={logo} width={60} height={60} />
@@ -14,8 +20,12 @@ function Header() {
         <IoMdSearch/>
         <input type='text' placeholder="Search games" className=' px-2 bg-transparent dark:bg-cyan-500 outline-none' />
       </div>
-      <div className='cursor-pointer' onClick={() => setToggle(!toggle)}>
-        {toggle ? <IoMoon className='text-[35px] bg-slate-200 text-black p-1 rounded-full items-center'/> : <MdSunny className='text-[35px] bg-slate-200 text-black p-1 rounded-full items-center'/>}
+      <div className='cursor-pointer' >
+        {theme== 'light' ? 
+          ( <IoMoon 
+            onClick={() => setTheme('dark')} className='text-[35px] bg-slate-200 text-black p-1 rounded-full items-center'/>
+          ) : ( <MdSunny 
+            onClick={() => setTheme('light')} className='text-[35px] bg-slate-200 text-black p-1 rounded-full items-center'/> )}
       </div>
     </div>
   )
