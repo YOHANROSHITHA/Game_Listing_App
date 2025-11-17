898da2691a03da91685b228388942dc04e9fc137
YOHANROSHITHA <yohanroshitha6@gmail.com>
2025-11-17 11:41:14 +0530

    implement dark mode support and update header styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import Header from './Components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [theme,setTheme] = useState('light')
 
   return (
-    <>
+    
+    <div className={ `${theme} ${theme=='dark' ?'bg-[#121212]' : null} `}>
     <h1 class="App">
       <Header/>
       <Home/>
     </h1>
 
     
-    </>
+    </div>
   )
 }
 
