003235cdd4aabc63641973f945ace3a371e90a63
YOHANROSHITHA <yohanroshitha6@gmail.com>
2025-11-24 01:41:16 +0530

    Refactor GenaralList component to include theme context and update genre list styling

--- src/Components/GenaralList.jsx
+++ src/Components/GenaralList.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import GlobalApi from '../Services/GlobalApi'
+import { ThemeContext } from '../Context/ThemeContext'
 
 function GenaralList() {
-
     const [genreList, setGenreList] = useState([])
+    const { theme } = useContext(ThemeContext); // Access the current theme
 
     // define the fetch function before useEffect so it's available when the effect runs
     const getGenreList = () => {
@@ -23,7 +24,18 @@ function GenaralList() {
 
   return (
     <div>
-      <h2 className='text-[30px] font-bold '>Genres</h2>
+      <h2
+                className={`text-[30px] font-bold ${ theme === 'dark' ? 'text-white' : 'text-black'}`}>
+                Genres
+      </h2>
+      {genreList.map((item)=>(
+        <div className='flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 rounded-lg'>
+            <img src={item.image_background} 
+            className='w-[40px] h-[40px] 
+            object-cover rounded-lg'/>
+            <h3 className='dark:text-white text-[18px]'>{item.name}</h3>
+        </div>
+      ))}
 
     </div>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,12 +4,13 @@ import GenaralList from '../Components/GenaralList'
 
 function Home() {
   return (
-    <div className='grid grid-cols-4'>
+    <div className='grid grid-cols-4 px-8'>
       <div className='md:block'>
         <GenaralList/>
       </div>
       <div className='col-span-4 md:col-span-3 bg-blue-500'>Game List</div>
-    </div>
+      
+      </div>
   )
 }
 
